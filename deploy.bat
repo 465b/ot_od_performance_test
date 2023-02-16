6977ba3469c7c035370c0eb6dfd6e91588019f9f
Laurin Steidle <laurin.steidle@uni-hamburg.de>
2023-02-17 11:16:11 +1300

    v02

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,11 @@
 @echo off
 
 rem Running the performance test
-call activate opendrift
-python ot_od_performance_test.py --model opendrift
-call deactivate
 
 call activate oceantracker
 python ot_od_performance_test.py --model oceantracker
+call deactivate
+
+call activate opendrift
+python ot_od_performance_test.py --model opendrift
 call deactivate
--- ot_od_performance_test.py
+++ ot_od_performance_test.py
@@ -23,7 +23,7 @@ cmd_parser = argparse.ArgumentParser(description='Run a test of the performance 
 cmd_parser.add_argument('--model', type=str, default='oceantracker', help='Which model to run. Options are "opendrift" and "oceantracker".')
 which_model = cmd_parser.parse_args().model
 
-name_of_run = 'schism_test'
+name_of_run = 'schism_test_v02'
 description_of_run = ''
 
 # input description
@@ -40,8 +40,48 @@ output_step_size = None
 
 # model description (solver, release, etc.name_of_run)
 
-release_points = [174.046669,-40.928116] # lon, lat
-release_points_schism = transformer.transform(release_points[0], release_points[1])
+release_points = np.array([
+       [173.786093  , -41.16104113],
+       [173.8069543 , -41.15268389],
+       [173.8240536 , -41.15088994],
+       [173.8394022 , -41.14940125],
+       [173.8627361 , -41.13468759],
+       [173.8588299 , -41.11545518],
+       [173.8414849 , -41.16840586],
+       [173.8252307 , -40.99551537],
+       [173.7977117 , -41.0284134 ],
+       [173.8563229 , -41.05863639],
+       [173.9248689 , -41.10665806],
+       [173.962755  , -41.13731895],
+       [173.9628523 , -41.11518312],
+       [174.0404062 , -41.05026245],
+       [174.0053709 , -41.03108366],
+       [173.9741537 , -41.01266866],
+       [173.9882097 , -40.98866365],
+       [173.9200421 , -40.97321731],
+       [173.963519  , -40.94149451],
+       [173.9313326 , -40.92225832],
+       [173.9810676 , -40.89785012],
+       [173.9006751 , -40.86638754],
+       [174.0972068 , -41.02928592],
+       [174.2013162 , -41.01473834],
+       [174.1787848 , -41.04393466],
+       [174.0854508 , -41.17965962],
+       [173.9946204 , -41.18222324],
+       [173.8869924 , -41.17131367],
+       [173.8869924 , -41.17131367],
+       [173.8821175 , -41.1505058 ],
+       [173.8821175 , -41.1505058 ],
+       [173.8704572 , -41.17645025],
+       [173.8962498 , -41.11146032],
+       [173.8638549 , -41.21179852],
+       [173.8638549 , -41.21179852],
+       [173.9329508 , -41.07649106],
+       [173.8833396 , -41.07830376],
+       [173.8833396 , -41.07830376]
+       ]) # lon, lat
+
+release_points_schism = transformer.transform(release_points[:,0], release_points[:,1])
 pulse_size = np.logspace(3,7,9,dtype=int)
 
 max_model_duration = 1 # days
@@ -49,11 +89,11 @@ max_model_duration = 1 # days
 # -----
 # ot represents the time step as sub steps. hence only ints allowed.
 # data time_step is 1800s (30min)
-model_time_step = 900 # seconds (15 min)
+model_time_step = 60 # seconds (1 min)
+
+RK_order = 4
 
 critical_resuspension_vel = 0
-
-
 
 
 if which_model=='oceantracker':
@@ -70,7 +110,7 @@ if which_model=='oceantracker':
     for pulse in pulse_size:
         params = {
             "shared_params": {
-                "output_file_base": name_of_run,
+                "output_file_base": name_of_run + '_ot',
                 "root_output_dir": path_to_output
             },
             "reader": {
@@ -82,28 +122,24 @@ if which_model=='oceantracker':
                 "run_params": {
                     "user_note": description_of_run,
                     "duration": max_model_duration*24*60*60,
+                    "write_tracks": False
                 },
                 "dispersion": {
                     "A_H": 0.1
                 },
-                "tracks_writer": {
-                    "class_name": "oceantracker.tracks_writer.track_writer_compact.FlatTrackWriter",
-                },
+                # "tracks_writer": {
+                #     "class_name": "oceantracker.tracks_writer.track_writer_compact.FlatTrackWriter",
+                # },
                 "solver": {
                     # "model_timestep": 1800.0,
                     "n_sub_steps": int(1800/model_time_step),
-                    "RK_order": 1, # open drift only supports 1st order
+                    "RK_order": RK_order,
                     "screen_output_step_count": 10,
                 },
                 "particle_release_groups": [
                     {
-                        "points": [
-                            [
-                                release_points_schism[0],
-                                release_points_schism[1]
-                            ]
-                        ],
-                        "pulse_size": pulse,
+                        "points": list([list(item) for item in np.array(release_points_schism).swapaxes(0,1)]),
+                        "pulse_size": int(pulse/len(release_points)),
                         "release_interval": 0
                     }
                 ],
@@ -132,8 +168,8 @@ if which_model=='oceantracker':
         total_time = int(total_time[0])*3600 + int(total_time[1])*60 + float(total_time[2])
 
         
-        print(f"| Number of particles | Total model time [m] |")
-        print(f"| {pulse: 19} | {total_time/60: 20.1f} |")
+        # print(f"| Number of particles | Total model time [m] |")
+        # print(f"| {pulse: 19} | {total_time/60: 20.1f} |")
 
         with open(os.path.join(path_to_output,name_of_run+'.txt'), 'a') as f:
             f.write(f"{which_model},{pulse},{total_time}\n")
@@ -172,14 +208,22 @@ if which_model == 'opendrift':
 
         # prevent opendrift from making a new dynamical landmask with global_landmask
         o.set_config('general:use_auto_landmask', False)
-
+        if RK_order == 4:
+            o.set_config('drift:advection_scheme', 'runge-kutta4')
+        elif RK_order == 1:
+            o.set_config('drift:advection_scheme', 'euler')
+        else:
+            print('Error: RK_order must be 1 or 4')
+            break   
         o.set_config('drift:horizontal_diffusivity',0.1) 
         o.set_config('vertical_mixing:resuspension_threshold', critical_resuspension_vel)
         o.set_config('vertical_mixing:diffusivitymodel', 'windspeed_Large1994')
 
 
         # Seed elements at defined positions, depth and time
-        o.seed_elements(lon=release_points[0], lat=release_points[1], radius=0, number=pulse,
+        for point in release_points:
+            o.seed_elements(lon=point[0], lat=point[1], radius=0,
+                        number=int(pulse/len(release_points)),
                         z=0,
                         time=schism_native.start_time)
 
@@ -188,14 +232,14 @@ if which_model == 'opendrift':
         #%%
         #%timeit
         o.run(end_time=schism_native.start_time + timedelta(days=max_model_duration), 
-            time_step=900, 
-            outfile=os.path.join(path_to_output,name_of_run+'.nc'))
+            time_step=900)
+            #outfile=os.path.join(path_to_output,name_of_run+'od.nc'))
         
         with open(os.path.join(path_to_output,name_of_run+'.txt'), 'a') as f:
             f.write(f"{which_model},{pulse},{o.timing['total time'].total_seconds()}\n")
 
-        print(f"| Number of particles | Total model time [m] |")
-        print(f"| {pulse: 19} | {o.timing['total time'].total_seconds()/60: 20.1f} |")
+        # print(f"| Number of particles | Total model time [m] |")
+        # print(f"| {pulse: 19} | {o.timing['total time'].total_seconds()/60: 20.1f} |")
 
         # o.plot(fast=True,filename=os.path.join(path_to_output,name_of_run+'od.png'))
         # o.animation(fast=True,filename=os.path.join(path_to_output,name_of_run+'od.mp4'))
